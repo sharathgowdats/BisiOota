0490812ffa8c306dbffe52956d6215598b6a4106
ST031851 <sharath.ts@cerner.com>
2018-08-29 16:59:07 +0530

    First Commit

---
+++ src/main/java/com/fouroddsolutions/bisioota/BisiOotaApplication.java
@@ -0,0 +1,12 @@
+package com.fouroddsolutions.bisioota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisiOotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisiOotaApplication.class, args);
+	}
+}
